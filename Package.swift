6f91fb6a9159ee69b40bb75256d311e7c54beb65
Andrii Hahan <andrii.h@appsflyer.com>
2021-07-05 13:27:24 +0300

    SPM duplicate target fix

--- Package.swift
+++ Package.swift
@@ -6,13 +6,13 @@ import PackageDescription
 let package = Package(
     name: "AppsFlyerLib",
     products: [
-        .library(name: "AppsFlyerLib", targets: ["AppsFlyerLib"]),
+        .library(name: "AppsFlyerLib", targets: ["AppsFlyerLib-Strict"]),
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
-            name: "AppsFlyerLib",
+            name: "AppsFlyerLib-Strict",
             url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.3.3/AppsFlyerLib-Strict.xcframework.zip",
             checksum: "bedca21ad8b04b3b5b6a4d4a967dc81bcc0f5b48ebb7b86e2f770c29df5d7f4c"
         ),

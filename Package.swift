d76ab0979f76f0705bf170af80a12631f5962e35
Andrii Hahan <andrii.h@appsflyer.com>
2021-07-05 13:08:27 +0300

    AppsFlyerFramework-Strict 6.3.3

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppsFlyerLib",
+    products: [
+        .library(name: "AppsFlyerLib", targets: ["AppsFlyerLib"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(name: "AppsFlyerLib", path: "AppsFlyerLib.xcframework"),
+        .binaryTarget(
+            name: "AppsFlyerLib",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.3.3/AppsFlyerLib-Strict.xcframework.zip",
+            checksum: "bedca21ad8b04b3b5b6a4d4a967dc81bcc0f5b48ebb7b86e2f770c29df5d7f4c"
+        ),
+    ]
+)

2f525e7abc4e5697588bd324f2b8bf55908a3b7c
Amit <amit.kremer@appsflyer.com>
2022-11-28 13:13:42 +0000

    Auto build: update version

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.9.0/AppsFlyerLib-Strict.xcframework.zip",
-            checksum: "baacc7799cdf5e1a70546a8456c01cc0df603d965247489ed9128b2d5e4d5539"
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.9.1/AppsFlyerLib-Strict.xcframework.zip",
+            checksum: "72fd76ee23e803a9c5c1a57b0d110bf8f669dddb515aee69a4fc476d9f10dec1"
         ),
     ]
 )

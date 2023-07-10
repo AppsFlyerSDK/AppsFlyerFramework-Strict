9a0e22da945b31eba873fe838c93a919efcec385
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-07-10 17:58:30 +0300

    Update SPM dependency to v6.12.0

--- Package.swift
+++ Package.swift
@@ -6,15 +6,13 @@ import PackageDescription
 let package = Package(
     name: "AppsFlyerLib-Strict",
     products: [
-        .library(name: "AppsFlyerLib-Strict", targets: ["AppsFlyerLib"]),
-    ],
-    dependencies: [
+        .library(name: "AppsFlyerLib-Strict", targets: ["AppsFlyerLib"])
     ],
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.11.0/AppsFlyerLib-Strict.xcframework.zip",
-            checksum: "a178f008330024f90f377a3c8cc663b2dbc10d47ededa0544513cb75b41a2cdd"
-        ),
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.0/AppsFlyerLib-Strict.xcframework.zip",
+            checksum: "207188125ad4d8807d2beb596ad6ce5981ffab9f03077a4284f0db2423936601"
+        )
     ]
 )

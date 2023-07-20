420c8622a7ee7f838f9d9ddb2a6e2b3fd7bf6ab4
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-07-20 16:36:19 +0300

    Package.swift file has been updated to 6.12.1

--- Package.swift
+++ Package.swift
@@ -1,18 +1,18 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "AppsFlyerLib-Strict",
+    name: "AppsFlyerLib",
     products: [
-        .library(name: "AppsFlyerLib-Strict", targets: ["AppsFlyerLib"])
+        .library(
+            name: "AppsFlyerLib-Strict",
+            targets: ["AppsFlyerLib"])
     ],
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.0/AppsFlyerLib-Strict.xcframework.zip",
-            checksum: "207188125ad4d8807d2beb596ad6ce5981ffab9f03077a4284f0db2423936601"
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.1/AppsFlyerLib-Strict.xcframework.zip",
+            checksum: "c1df657e60fd8d9f66b521ec03e8c71580d120a56f9e66467c3aae708b5bd381"
         )
     ]
 )

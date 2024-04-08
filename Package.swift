f9e6a597252ab719805033a7912c64d2dd1f077e
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2024-04-08 13:38:22 +0300

    Update strict framework path for v6.14.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.14.0/AppsFlyerLib-Strict.xcframework.zip",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.14.0/AppsFlyerLib-Strict-SPM.xcframework.zip",
             checksum: "6af5b232f408cd6d89be3d039367169b6321610f057d1e763a0eb68855b1dbc6"
         )
     ]

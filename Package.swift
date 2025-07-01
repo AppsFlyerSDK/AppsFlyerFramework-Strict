534a8e2ca2e55d3dc5b9c9655e43900582effad5
Ivan Obodovskyi <ivan.obodovskyi@appsflyer.com>
2025-07-01 13:17:27 +0300

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.17.0/AppsFlyerLib-Strict-SPM.xcframework.zip",
-            checksum: "abe5e16cc7a601dac1f62cdbfca42b9e8f313e5a2b8f536fe67a3024a9366d59"
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.17.1/AppsFlyerLib-Strict-SPM.xcframework.zip",
+            checksum: "26276b0fb48149a56d22316ff0222f6ae21dceab5373eb3eb4ba7135cb29cf71"
         )
     ]
 )

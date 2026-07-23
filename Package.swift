1b900eade06640715b6bb221e59fdc0748e28e86
ec2-user <ec2-user@ip-10-10-86-122.eu-west-1.compute.internal>
2026-07-23 05:28:55 +0000

    Package.swift file have been updated to 7.0.1

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/7.0.0/AppsFlyerLib-Strict-SPM.xcframework.zip",
-            checksum: "676f8fb337a04f816f0a687756328d14992c1d45678427277368f62359e49fdb"
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/7.0.1/AppsFlyerLib-Strict-SPM.xcframework.zip",
+            checksum: "a83fa594fe4c4adcbcf6f203260920cbb93f55323446c107283d2fa1d96e8c82"
         )
     ]
 )
